6715d197a104ffb19fffb3b0caae92fa5730640a
s-brez <sam@sdbgroup.io>
2021-09-11 16:38:25 +1000

    add smaller tree test for kdtree

--- nearestNeigh/kd_tree_nn.py
+++ nearestNeigh/kd_tree_nn.py
@@ -13,6 +13,7 @@ class KDTreeNN(NearestNeigh):
     def build_index(self, points: [Point]):
 
         self.root = self.kdtree(points, 0, None)
+        self.print_count = 0
 
     def kdtree(self, points: [Point], axis, parent=None) -> Node:
 
@@ -60,7 +61,13 @@ class KDTreeNN(NearestNeigh):
 
         self.nns, self.closed_list = [], []
         self.search_knn(None, self.root, search_term, k, 0, None)
+
         self.nns = sorted(self.nns, key=lambda k: k['dist'])
+
+        print("neighbours:")
+        for n in [(n['point'].id, n['dist']) for n in self.nns]:
+            print(n)
+
         self.nns = [n['point'] for n in self.nns]
 
         return self.nns
@@ -79,42 +86,49 @@ class KDTreeNN(NearestNeigh):
 
         if C is not None:
 
+            input("----------search_knn()---------")
+
             prev_print_val = prev.point.id if prev else None
             dtn = "Traversed to" if not backtracking else "Backtracked to"
-            # print(dtn, C.point.id, "from", prev_print_val)
+            children = ""
+            if C.left:
+                children += str("L: " + C.left.point.id)
+            if C.right:
+                children += str(", R: " + C.right.point.id)
 
-            # Stop backtracking at when root reached
-            if C.parent is None and backtracking:
-                return
+            print(dtn, C.point.id, "from", prev_print_val)
+            print(C.point.id, "split on", C.was_split_on)
+            print(C.point.id, "has children:", children)
 
             # Prevent uneccesary evaluations/multiple traversals
             c_hash = self.hash_point(C.point)
             if c_hash in self.closed_list:
-                # print(prev.point.id, "to", C.point.id, "already traversed")
+                print(prev.point.id, "to", C.point.id, "already traversed")
                 return
 
             # Get relevant per-axis node and distance values
-            check_other_subtree = False
             a_dist = C.point.dist_to(T)
             T_dim_val = T.lat if axis % 2 == 0 else T.lon
-            C_dim_val = C.point.lat if axis % 2 == 0 else T.lat
+            C_dim_val = C.point.lat if axis % 2 == 0 else C.point.lon
 
             # Forward traverse - follow dim splits to leaf nodes
             if not backtracking:
                 if T_dim_val >= C_dim_val:
                     if C.right is not None:
+                        print("Going to", C.point.id, "right child:", C.right.point.id, T_dim_val, ">=", C_dim_val)
                         self.search_knn(C, C.right, T, k, axis + 1)
                     else:
                         backtracking = True
                 else:
                     if C.left is not None:
+                        print("Going to", C.point.id, "left child:", C.left.point.id, T_dim_val, "<", C_dim_val)
                         self.search_knn(C, C.left, T, k, axis + 1)
                     else:
                         backtracking = True
 
                 if not C.left and not C.right:
                     backtracking = True
-                    # print(C.point.id, "leaf node reached")
+                    print(C.point.id, "leaf node reached")
 
             # Reverse - evaluate C
             if backtracking:
@@ -122,22 +136,23 @@ class KDTreeNN(NearestNeigh):
                 p_dist = self.p_distance(axis, C.point, T)
                 if C.point.cat == T.cat:
                     if len(self.nns) < k:
+                        print("saving as less than k nns exist")
                         self.save_n(a_dist, C.point)
-                        check_other_subtree = True
                     elif len(self.nns) == k and self.nns[0]['dist'] > a_dist:
+                        print("saving as less than k nns exist")
                         self.save_n(a_dist, C.point, replace=True)
-                        check_other_subtree = True
 
                 # Fwd-traverse unexplored subtree if required
-                if self.nns[0]['dist'] > p_dist or len(self.nns) < k or check_other_subtree:
-                    # print("checking", C.point.id, "other subtree")
+                # if self.nns[0]['dist'] > p_dist or len(self.nns) < k or check_other_subtree:
+                if self.nns[0]['dist'] > p_dist:
+                    self.closed_list.append(c_hash)
+                    print(C.point.id, "splitting line is closer to target axis than furthest neighbour", self.nns[0]['dist'], ">", p_dist)
                     if C.left is not None:
                         self.search_knn(C, C.left, T, k, axis + 1)
                     if C.right is not None:
                         self.search_knn(C, C.right, T, k, axis + 1)
 
                 # Continue backwards up the tree
-                self.closed_list.append(c_hash)
                 if C.parent is not None:
                     self.search_knn(C, C.parent, T, k, axis - 1,
                                     backtracking=True)
@@ -164,10 +179,6 @@ class KDTreeNN(NearestNeigh):
                 self.nns.append(new_n)
                 self.nns = sorted(self.nns, key=lambda k: k['dist'],
                                   reverse=True)
-
-            print("neighbours:")
-            for n in [(n['point'].id, n['dist']) for n in self.nns]:
-                print(n)
 
     def add_point(self, point: Point) -> bool:
         """
@@ -237,7 +248,13 @@ class KDTreeNN(NearestNeigh):
 
         if depth == 1:
             self.print_count += 1
-            print(node.point, self.print_count)
+            children = ""
+            if node.left:
+                children += str("L: " + node.left.point.id)
+            if node.right:
+                children += str(", R: " + node.right.point.id)
+
+            print(node.point, "split:", node.was_split_on, "children:", children)
 
         elif depth > 1:
             self.print_level(node.left, depth - 1)
--- nearestNeigh/naive_nn.py
+++ nearestNeigh/naive_nn.py
@@ -45,8 +45,8 @@ class NaiveNN(NearestNeigh):
         # Slice for k neighbours
         k_neighbours = [i['point'] for i in sorted_distances[:k]]
 
-        for n in sorted_distances:
-            print(n['point'].id, "dist:", n['dist'])
+        # for n in sorted_distances:
+        #     print(n['point'].id, "dist:", n['dist'])
 
         return k_neighbours
 
